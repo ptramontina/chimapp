f8764dae3ce4275aa64c8fd92a5453818b6f5840
Pedro Ernesto Tramontina <pedro.tramontina@gmail.com>
2020-11-16 19:29:05 -0300

    Remove unnecessary code

--- web/src/db/mongodb.js
+++ web/src/db/mongodb.js
@@ -1,24 +1,3 @@
-// const MongoClient = require('mongodb').MongoClient;  
-// const assert = require('assert');
-
-// // This is a global variable we'll use for handing the MongoDB client
-// let mongodb;
-
-// // Connection URL
-// let url = 'mongodb://127.0.0.1:27017/chimapp-api';
-
-// // Create the db connection
-// MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {  
-//         assert.equal(null, err);
-//         this.mongodb = db;
-//         console.log('iii')
-//         console.log(this.mongodb)
-//     }
-// );
-
-// module.exports = mongodb;
-
-
 const MongoClient = require( 'mongodb' ).MongoClient;
 const url = 'mongodb://127.0.0.1:27017/chimapp-api';
 
